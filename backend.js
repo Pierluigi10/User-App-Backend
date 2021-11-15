240c4e246ec0b30f62ccb9be2947a4cd01216b73
pierluigi <pierluigibaiano@gmail.com>
2021-11-15 13:14:58 +0100

    Live user app Backend

---
+++ backend.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+
+const app = express();
+const port = 3022;
+
+const mongoConnectionsString = "mongodb://localhost:27017";
+const client = new MongoClient(mongoConnectionsString);
+
+const getData = async (done) => {
+  await client.connect();
+  const db = client.db("api001");
+  done(db);
+};
+
+// app.get('/', (req, res) => {
+//     res.send("it works(TEST)")
+// })
+
+app.get("/", (req, res) => {
+  getData(async (db) => {
+    const users = await db.collection("users100").find()
+    .project({
+      name: 1,
+      username: 1,
+      email: 1
+    }).toArray();
+    res.json(users);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listen on http://localhost:${port}`);
+});
